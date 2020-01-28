430031f1b4256bb3d9b80a0c5b4a92e9204b96bf
jstapleton463 <42473264+jstapleton463@users.noreply.github.com>
2020-01-28 09:15:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# A starter program for Python with Tkinter
+
+from tkinter import * # import Tkinter library
+window = Tk()         # Create the application window
+
+# Add a label with the text "Hello"
+lbl = Label(window, text="Hello")
+
+# Place the label in the window at 0, 0
+lbl.grid(column=0, row=0)
+
+
+window.mainloop()     # Keep the window open
