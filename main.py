6b9bd4702143d2a78e402963ec8628d98c0fda35
xavier wright <xmwrig79>
2020-01-28 11:09:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
+lbl = Label(window, text=" I am Xavier Wright")
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
