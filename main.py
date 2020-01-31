56f7ff1dbf6e9189b04f083f78253891ac118631
Jon <jstapleton463@harrisonburg.k12.va.us>
2020-01-31 10:39:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,27 @@
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
-# Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
+score = 0
 
-# Place the label in the window at 0, 0
+def addToScore():
+  message = txt.get()
+  if message == "Jon":
+    lbl['text'] = "go away"
+  else:
+    lbl['text'] = "hello"
+
+# Add a label with the text "Hello"
+lbl = Label(window, text=score, font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Click", command=addToScore)
+btn.grid(column = 0 , row = 1)
+
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+
 
 
 window.mainloop()     # Keep the window open
+
+
